c366fc97c18ef4290b232731b23a716c6ae3c5b4
chhsiao93 <chhsiao@utexas.edu>
2024-05-24 12:58:41 -0500

    added the ipynb for nerfstudio

--- read_pcd.py
+++ read_pcd.py
@@ -2,7 +2,7 @@
 import numpy as np
 import open3d as o3d
 
-pcd = o3d.io.read_point_cloud('cylinder_t0.ply')
+pcd = o3d.io.read_point_cloud('sandbox_tk.ply')
 print(pcd)
 print(np.asarray(pcd.points).max(axis=0))
 print(np.asarray(pcd.points).min(axis=0))
